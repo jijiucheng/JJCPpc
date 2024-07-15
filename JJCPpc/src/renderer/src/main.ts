1b8d165fdf58072bbec665f76607fc98f6513d7f
jcji <302926124@qq.com>
2024-07-15 10:38:07 +0800

    perf：1、优化代码结构

--- JJCPpc/src/renderer/src/main.ts
+++ JJCPpc/src/renderer/src/main.ts
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import pinia from '@/stores/index'
+import { createPinia } from 'pinia'
 
 import '@/scss/base/base.scss'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
 const app = createApp(App)
-app.use(pinia)
+app.use(createPinia())
 app.use(ElementPlus)
 app.mount('#app')
